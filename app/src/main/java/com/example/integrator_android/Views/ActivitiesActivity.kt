3b41c3a210d9b798d590d51b829ba2a0bac78c13
Mateo <mateo.lescano@mercadolibre.com>
2022-06-06 16:36:28 -0300

    Initial Setup

---
+++ app/src/main/java/com/example/integrator_android/Views/ActivitiesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.integrator_android.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitiesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activities)
+    }
+}
---
+++ app/src/main/java/com/example/integrator_android/Views/TyCActivity.kt
@@ -0,0 +1,11 @@
+package com.example.integrator_android.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TyCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ty_cactivity)
+    }
+}
