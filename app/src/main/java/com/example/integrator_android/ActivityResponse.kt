0f284467ad0218ca833a55a451298fe7b82c60cc
Ignacio Lambert <ilambert@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-06-06 17:02:03 -0300

    Agrego APIService y ActivityResponse

---
+++ app/src/main/java/com/example/integrator_android/ActivityResponse.kt
@@ -0,0 +1,8 @@
+package com.example.integrator_android
+
+import com.google.gson.annotations.SerializedName
+
+data class ActivityResponse (
+    //TODO hay que cambiar estos parametros,lo deje asi para que ande el APIService
+    @SerializedName("message") var activityList: List<String>
+)
