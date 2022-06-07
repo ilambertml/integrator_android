0b114db04fc5ca926315fea6ef5439420b358e2a
Brenda Rodriguez <brendamicaela.rodriguez@mercadolibre.com>
2022-06-07 11:30:31 -0300

    Add back button in SuggestionsActivity.kt

--- app/src/main/java/com/example/integrator_android/Views/SuggestionsActivity.kt
+++ app/src/main/java/com/example/integrator_android/Views/SuggestionsActivity.kt
@@ -1,12 +1,29 @@
 package com.example.integrator_android.Views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import com.example.integrator_android.R
+
 
 class SuggestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suggestions)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true) // showing the back button in action bar
+    }
+
+    // this event will enable the back function to the button on press:
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
