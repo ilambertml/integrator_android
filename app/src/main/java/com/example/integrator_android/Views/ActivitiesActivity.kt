73bc7ecdde2227d3996ee996eac7cc0f2c67b66d
Mateo <mateo.lescano@mercadolibre.com>
2022-06-07 12:05:33 -0300

    Extracted Strings

--- app/src/main/java/com/example/integrator_android/Views/ActivitiesActivity.kt
+++ app/src/main/java/com/example/integrator_android/Views/ActivitiesActivity.kt
@@ -4,22 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.integrator_android.ActivitiesListAdapter
+import com.example.integrator_android.R
 import com.example.integrator_android.databinding.ActivityActivitiesBinding
 
 class ActivitiesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityActivitiesBinding
-    private var activitiesList: MutableList<String> = mutableListOf()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityActivitiesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        for (i in 1..20){
-            activitiesList.add(i.toString())
-        }
+        val activitiesList: MutableList<String> = mutableListOf(getString(R.string.actRandom),
+            getString(R.string.actEducation), getString(R.string.actRec), getString(R.string.actSocial),
+            getString(R.string.actDIY), getString(R.string.actCharity), getString(R.string.actCooking),
+            getString(R.string.actRelaxation), getString(R.string.actMusic), getString(R.string.actBusywork))
 
         val listAdapter = ActivitiesListAdapter(this,activitiesList)
         binding.activitiesLV.adapter = listAdapter
@@ -28,10 +28,21 @@ class ActivitiesActivity : AppCompatActivity() {
             navigateSuggestions()
         }
 
+        supportActionBar?.apply {
+            title = getString(R.string.titleActivities)
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
     }
 
     private fun navigateSuggestions(){
         val intent = Intent(this, SuggestionsActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
